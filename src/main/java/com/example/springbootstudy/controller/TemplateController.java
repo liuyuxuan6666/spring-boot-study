1d8b64208e32f815fff22235adca4540f9e07ccb
lyx <1098659745@qq.com>
2023-08-23 11:52:33 +0800

    init

---
+++ src/main/java/com/example/springbootstudy/controller/TemplateController.java
@@ -0,0 +1,10 @@
+package com.example.springbootstudy.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 不加REST的controller，返回的是模板
+ */
+@Controller
+public class TemplateController {
+}
